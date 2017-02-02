6066cc8bb20122a16d60e9c14332fbf6a396297b
Chris Gueco <chris.gueco@gmail.com>
2017-02-01 18:52:02 -0800

    it's starting to look like a real thing!

--- app/src/js/components/HeroStats.js
+++ app/src/js/components/HeroStats.js
@@ -1,45 +1,108 @@
 import React, { Component, PropTypes } from 'react';
 
+import { Heroes, GetHeroDetails } from './../modules/heroes';
 
 import Stat from './Stat';
 
 class HeroStats extends Component {
-
-
-
 	render () {
-		switch ( this.props.heroValue ) {
-			case '0x02E0000000000003':
-				return < TracerStats stats={ this.props.stats } />
-		}
-		return <div>
+
+		let inner = null;
+
+		// switch ( this.props.heroValue ) {
+		// 	case '0x02E0000000000003':
+		// 		inner = < TracerStats details={ GetHeroDetails('0x02E0000000000003') }
+		// 							  stats={ this.props.stats } />
+		// 		break;
+		// 	default:
+		// 		inner = <div> </div>
+		// }
+
+		inner = <TracerStats details={ GetHeroDetails( this.props.heroValue )}  stats={this.props.stats } />
+
+		return <div className="heroStats">
+			{ inner }
+			<div className="heroStats-card">Hello</div>
 		</div>
 	}
-
-
 }
 
 export default HeroStats
 
 
-
 export class TracerStats extends Component {
 	render () {
 		let { stats } = this.props;
-		let statComponents = Object.keys( stats.Game ).map( key => {
-			let { name, value } = stats.Game[ key ];
-			return <Stat
-						key={ key }
-						name={ key }
-						value={ value }
-						label={ name } />
+		let { hero, details } = this.props.details;
+		let categories = details.display.map( _category => {
+			return <StatCollectionContainer
+						key={ _category.icon }
+						stats={ stats }
+						paths={ _category.statPaths }
+						image={ hero.images[_category.icon ]} />
 		});
 
-
-		return <div>
-			{ statComponents }
+		return <div className="heroStats-main">
+			{ categories }
 		</div>
 	}
 }
 
+const StatCollectionContainer = ({ image, paths, stats }) => {
+	return (
+		<div className="heroStats-card">
+			<div className="heroStats-statCollectionContainer">
+				<AbilityIcon image={ image } />
+				<StatCollection paths={ paths } stats={ stats } />
+			</div>
+		</div>
+	)
+}
 
+const AbilityIcon = ({ image }) => {
+	return (
+		<div className="heroStats-abilityIcon">
+			<img src={ image } />
+		</div>
+	)
+}
+
+const StatCollection = ({ paths, stats }) => {
+	return  (
+		<div className="heroStats-statCollection">
+			{
+				paths.map( _path  => {
+					let stat = getStatFromPath( stats, _path );
+					if ( !stat ) {
+						return null;
+					}
+					return <HeroStat key={ stat.name } stat={stat} />
+				})
+			}
+		</div>
+	)
+}
+
+const HeroStat = ({ stat }) => {
+	return (
+		<div className="heroStats-stat">
+			<span className="heroStats-stat-name">{ stat.name }</span>
+			<span className="heroStats-stat-value">{ stat.value }</span>
+		</div>
+	)
+}
+
+const HeroStatProgress = ({ value, total }) => {
+	return (
+		<div className="heroStats-statProgress">
+			<div className="heroStats-statProgress-inner"></div>
+		</div>
+	)
+}
+
+function getStatFromPath ( stats, path ) {
+	let segments = path.split('.');
+	let stat = stats[ segments.shift() ];
+	segments.forEach( ( seg ) => { stat = stat[ seg ] });
+	return stat;
+}
--- app/src/js/modules/heroes.js
+++ app/src/js/modules/heroes.js
@@ -1,4 +1,7 @@
 export const Heroes = {
+
+
+
 	"0x02E00000FFFFFFFF": {
 		name: 'All Heroes',
 		color: 'rgb(0, 0, 0)',
@@ -7,6 +10,13 @@ export const Heroes = {
 	"0x02E0000000000002": {
 		name: 'Reaper',
 		color: 'rgb(39, 39, 37)',
+		images: {
+			icon: 'https://firebasestorage.googleapis.com/v0/b/gopherwatch-664c5.appspot.com/o/reaper%2Freaper-icon.png?alt=media&token=14433501-7b5c-4070-a344-8ed1cea2a5dd',
+			primary: 'https://firebasestorage.googleapis.com/v0/b/gopherwatch-664c5.appspot.com/o/reaper%2Freaper-primary.png?alt=media&token=d384728d-307e-4f75-b576-3ec0461e8a53',
+			teleport: 'https://firebasestorage.googleapis.com/v0/b/gopherwatch-664c5.appspot.com/o/reaper%2Freaper-teleport.png?alt=media&token=c76432b6-2ebc-4c5b-b552-b69ce1cebcde',
+			ult: 'https://firebasestorage.googleapis.com/v0/b/gopherwatch-664c5.appspot.com/o/reaper%2Freaper-ult.png?alt=media&token=4b424c53-5d05-4601-b69b-5462f944747d',
+			wraith: 'https://firebasestorage.googleapis.com/v0/b/gopherwatch-664c5.appspot.com/o/reaper%2Freaper-wraith.png?alt=media&token=fd07805a-5236-4f97-ab10-4cb8c9affdfc'
+		}
 	},
 	"0x02E0000000000005": {
 		name: "Hanzo",
@@ -14,58 +24,97 @@ export const Heroes = {
 	},
 	"0x02E0000000000003": {
 		name: 'Tracer',
-		portrait: 'https://firebasestorage.googleapis.com/v0/b/gopherwatch-664c5.appspot.com/o/tracer-portrait.png?alt=media&token=7fe29909-0081-4bd9-b6ea-7eea6020e35a',
 		color: 'rgb(248, 145, 27)',
-		icon: 'https://firebasestorage.googleapis.com/v0/b/gopherwatch-664c5.appspot.com/o/tracer-icon.png?alt=media&token=3cfbcfe7-83ff-4be3-85ed-f4a548eb4a0c',
+		images: {
+			icon: 'https://firebasestorage.googleapis.com/v0/b/gopherwatch-664c5.appspot.com/o/tracer-icon.png?alt=media&token=3cfbcfe7-83ff-4be3-85ed-f4a548eb4a0c',
+			pulsePistols: 'https://firebasestorage.googleapis.com/v0/b/gopherwatch-664c5.appspot.com/o/tracer-primary.png?alt=media&token=e80540ef-2704-4145-84be-262b4650f140',
+			pulseBomb: 'https://firebasestorage.googleapis.com/v0/b/gopherwatch-664c5.appspot.com/o/tracer-ult.png?alt=media&token=9864416e-dc2d-43a2-959e-f5a7a88e0e88',
+			rewind: 'https://firebasestorage.googleapis.com/v0/b/gopherwatch-664c5.appspot.com/o/tracer-rewind.png?alt=media&token=29477663-c9e8-4482-9584-3b50c58c14fd',
+			portrait: 'https://firebasestorage.googleapis.com/v0/b/gopherwatch-664c5.appspot.com/o/tracer-portrait.png?alt=media&token=7fe29909-0081-4bd9-b6ea-7eea6020e35a',
+		},
 		abilities: {
-			primary: {
+			"pulsePistols": {
 				name: "Pulse Pistols",
 				type: 'primary',
-				icon: 'https://firebasestorage.googleapis.com/v0/b/gopherwatch-664c5.appspot.com/o/tracer-primary.png?alt=media&token=e80540ef-2704-4145-84be-262b4650f140',
 			},
-			ultimate: {
+			"pulseBomb": {
 				name: "Pulse Bomb",
 				type: "ultimate",
-				icon: 'https://firebasestorage.googleapis.com/v0/b/gopherwatch-664c5.appspot.com/o/tracer-ult.png?alt=media&token=9864416e-dc2d-43a2-959e-f5a7a88e0e88'
 			},
-			secondary: [
-				{
-					name: 'Rewind',
-					type: 'secondary',
-					icon: 'https://firebasestorage.googleapis.com/v0/b/gopherwatch-664c5.appspot.com/o/tracer-rewind.png?alt=media&token=29477663-c9e8-4482-9584-3b50c58c14fd'
-				}
-			],
+			"rewind": {
+				name: 'Rewind',
+				type: 'secondary',
+			}
 		}
-},
+	},
 	"0x02E0000000000004": {
 		name: 'Mercy',
 		color: 'rgb(255, 225, 108)',
-		ultIcon: 'https://firebasestorage.googleapis.com/v0/b/gopherwatch-664c5.appspot.com/o/0x0E60000000000187.png?alt=media&token=e9b6ec42-bbdf-43f3-af5c-6728e90e8d2d'
+		images: {
+			float: 'https://firebasestorage.googleapis.com/v0/b/gopherwatch-664c5.appspot.com/o/mercy%2Fmercy-descent.png?alt=media&token=5e0d58b0-83f5-4c6f-a017-1549027bdeab',
+			icon: 'https://firebasestorage.googleapis.com/v0/b/gopherwatch-664c5.appspot.com/o/mercy%2Fmercy-guardian.png?alt=media&token=9ab4589e-322b-43dc-9448-2561d2205685',
+			caduceus: 'https://firebasestorage.googleapis.com/v0/b/gopherwatch-664c5.appspot.com/o/mercy%2Fmercy-primary-2.png?alt=media&token=bb17f1d5-2979-4455-b8bc-6bd328617ba5',
+			heal: 'https://firebasestorage.googleapis.com/v0/b/gopherwatch-664c5.appspot.com/o/mercy%2Fmercy-primary.png?alt=media&token=63b30a7b-6fb3-4403-8862-01dc43c1facd',
+			rez: 'https://firebasestorage.googleapis.com/v0/b/gopherwatch-664c5.appspot.com/o/mercy%2Fmercy-rez.png?alt=media&token=30c98ecb-214d-4571-b8bb-8c78db7d1b3a'
+		},
 	},
 	"0x02E00000000000DD": {
 		name: 'Mei',
 		color: 'rgb(154, 219, 244)',
-		ultIcon: 'https://firebasestorage.googleapis.com/v0/b/gopherwatch-664c5.appspot.com/o/0x0E60000000000174.png?alt=media&token=b329b9ea-72ba-4358-80f9-05f52aeb3e78'
+		images: {
+			freeze: 'https://firebasestorage.googleapis.com/v0/b/gopherwatch-664c5.appspot.com/o/mei%2Fmei-freeze.png?alt=media&token=56a6504b-6d20-4b19-9d15-1268581c6ffa',
+			icon: 'https://firebasestorage.googleapis.com/v0/b/gopherwatch-664c5.appspot.com/o/mei%2Fmei-icon.png?alt=media&token=78607526-b09f-4840-8473-b328a1a5a9c3',
+			primary: 'https://firebasestorage.googleapis.com/v0/b/gopherwatch-664c5.appspot.com/o/mei%2Fmei-primary.png?alt=media&token=82fe4297-6714-4871-9f60-b104a737f9ee',
+			bluzzard: 'https://firebasestorage.googleapis.com/v0/b/gopherwatch-664c5.appspot.com/o/mei%2Fmei-ult.png?alt=media&token=2a7f8bcf-c23a-420a-b58f-0243f7c92c1b',
+			wall: 'https://firebasestorage.googleapis.com/v0/b/gopherwatch-664c5.appspot.com/o/mei%2Fmei-wall.png?alt=media&token=9b3427c7-712a-45f0-9d91-b6929a595bd5'
+		}
 	},
 	"0x02E000000000007A": {
 		name: 'D.Va',
 		color: 'rgb(255, 127, 209)',
-		ultIcon: 'https://firebasestorage.googleapis.com/v0/b/gopherwatch-664c5.appspot.com/o/0x0E6000000000017B.png?alt=media&token=96a402e0-5de0-482d-8656-948610b59945'
+		images: {
+			boosters: 'https://firebasestorage.googleapis.com/v0/b/gopherwatch-664c5.appspot.com/o/dva%2Fdva-booster.png?alt=media&token=d1748147-189b-4f0d-b7ae-27bb1d7cf3bf',
+			icon: 'https://firebasestorage.googleapis.com/v0/b/gopherwatch-664c5.appspot.com/o/dva%2Fdva-icon.png?alt=media&token=1f9a6b55-c2b8-407e-9fd4-3d3c3deece03',
+			matrix: 'https://firebasestorage.googleapis.com/v0/b/gopherwatch-664c5.appspot.com/o/dva%2Fdva-matrix.png?alt=media&token=7dab3f6a-e4aa-43a7-b53a-33ab521985b9',
+			mechPrimary: 'https://firebasestorage.googleapis.com/v0/b/gopherwatch-664c5.appspot.com/o/dva%2Fdva-mech-primary.png?alt=media&token=73672dc7-62b4-4b38-ae70-8292c693796a',
+			mech: 'https://firebasestorage.googleapis.com/v0/b/gopherwatch-664c5.appspot.com/o/dva%2Fdva-mech.png?alt=media&token=0a4bb3f6-d412-438a-aa5f-594418ccdce3',
+			primary: 'https://firebasestorage.googleapis.com/v0/b/gopherwatch-664c5.appspot.com/o/dva%2Fdva-primary.png?alt=media&token=d03880a9-752a-4c3b-9e63-7b23870643f3',
+			selfDestruct: 'https://firebasestorage.googleapis.com/v0/b/gopherwatch-664c5.appspot.com/o/dva%2Fdva-ult.png?alt=media&token=cba96917-7fcf-44be-af7a-fbe11663f304',
+		}
 	},
 	"0x02E0000000000007": {
 		name: 'Reinhardt',
 		color: 'rgb(170, 149, 142)',
-		ultIcon: 'https://firebasestorage.googleapis.com/v0/b/gopherwatch-664c5.appspot.com/o/0x0E6000000000017C%20(1).png?alt=media&token=8b261313-4a3f-4e03-b6fa-4c5d7c6de0e3'
+		images: {
+			barrier: 'https://firebasestorage.googleapis.com/v0/b/gopherwatch-664c5.appspot.com/o/reinhardt%2Frein-barrier.png?alt=media&token=cc840399-a408-414d-b4dd-477c7c11ac2c',
+			charge: 'https://firebasestorage.googleapis.com/v0/b/gopherwatch-664c5.appspot.com/o/reinhardt%2Frein-charge.png?alt=media&token=6af0fb66-08a9-4b72-843f-7176eff84526',
+			icon: 'https://firebasestorage.googleapis.com/v0/b/gopherwatch-664c5.appspot.com/o/reinhardt%2Frein-icon.png?alt=media&token=ae9df48d-c64d-4ecc-9dc5-bfb3f297fbda',
+			primary: 'https://firebasestorage.googleapis.com/v0/b/gopherwatch-664c5.appspot.com/o/reinhardt%2Frein-primary.png?alt=media&token=3eac5483-baa9-43ba-a91b-c20f66392542',
+			fireStrike: 'https://firebasestorage.googleapis.com/v0/b/gopherwatch-664c5.appspot.com/o/reinhardt%2Frein-strike.png?alt=media&token=31a6f447-49ed-4e5d-a1f6-7a595d12bc71',
+			ult: 'https://firebasestorage.googleapis.com/v0/b/gopherwatch-664c5.appspot.com/o/reinhardt%2Frein-ult.png?alt=media&token=0899ce96-d5dc-418e-a72f-351ff36b40fa',
+		}
 	},
 	"0x02E0000000000016": {
 		name: 'Symmetra',
 		color: 'rgb(92, 236, 255)',
-		ultIcon: 'https://firebasestorage.googleapis.com/v0/b/gopherwatch-664c5.appspot.com/o/0x0E60000000000189.png?alt=media&token=f6f8e18d-2d01-4b5d-8a55-b8924fd974e6'
+		images: {
+			armor: 'https://firebasestorage.googleapis.com/v0/b/gopherwatch-664c5.appspot.com/o/symmetra%2Fsym-armor.png?alt=media&token=7f78f165-db42-47a7-9563-192272557f10',
+			barrier: 'https://firebasestorage.googleapis.com/v0/b/gopherwatch-664c5.appspot.com/o/symmetra%2Fsym-barrier.png?alt=media&token=1831150e-c892-49f7-ae61-9c350a388490',
+			icon: 'https://firebasestorage.googleapis.com/v0/b/gopherwatch-664c5.appspot.com/o/symmetra%2Fsym-icon.png?alt=media&token=b81ac12e-f5cf-4da2-acda-7665a4029781',
+			primary: 'https://firebasestorage.googleapis.com/v0/b/gopherwatch-664c5.appspot.com/o/symmetra%2Fsym-primary.png?alt=media&token=4ccf5d1b-32eb-45d8-adc0-128358e4f5f8',
+			teleport: 'https://firebasestorage.googleapis.com/v0/b/gopherwatch-664c5.appspot.com/o/symmetra%2Fsym-turret.png?alt=media&token=939b081c-de05-4783-9215-6aaffb46f283',
+		}
 	},
 	"0x02E0000000000079": {
 		name: 'Lucio',
 		color: 'rgb(139, 236, 34)',
-		ultIcon: 'https://firebasestorage.googleapis.com/v0/b/gopherwatch-664c5.appspot.com/o/0x0E60000000000185.png?alt=media&token=dec1c7a8-d318-4491-9001-e4abae41740b'
+		images: {
+			amp: 'https://firebasestorage.googleapis.com/v0/b/gopherwatch-664c5.appspot.com/o/lucio%2Flucio-amp.png?alt=media&token=7920a340-778b-4bbf-a57e-eb70c9c453d1',
+			crossFade: 'https://firebasestorage.googleapis.com/v0/b/gopherwatch-664c5.appspot.com/o/lucio%2Flucio-crossfade.png?alt=media&token=6ec5f6a0-542a-4df7-9f50-44d1cad01562',
+			icon: 'https://firebasestorage.googleapis.com/v0/b/gopherwatch-664c5.appspot.com/o/lucio%2Flucio-icon.png?alt=media&token=23aefeb3-c1c0-436c-aef3-375861bf049c',
+			boop: 'https://firebasestorage.googleapis.com/v0/b/gopherwatch-664c5.appspot.com/o/lucio%2Flucio-primary.png?alt=media&token=99102016-45e7-4e0d-9df1-08890871cf8f',
+			ult: 'https://firebasestorage.googleapis.com/v0/b/gopherwatch-664c5.appspot.com/o/lucio%2Flucio-ult.png?alt=media&token=1fa7edc4-6e5f-453a-ab1e-58ed548bb801'
+		}
 	},
 	"0x02E000000000006E": {
 		name: 'Soldier: 76',
@@ -75,7 +124,13 @@ export const Heroes = {
 	"0x02E0000000000068": {
 		name: "Zarya",
 		color: 'rgb(245, 113, 168)',
-		ultIcon: 'https://firebasestorage.googleapis.com/v0/b/gopherwatch-664c5.appspot.com/o/0x0E60000000000185.png?alt=media&token=dec1c7a8-d318-4491-9001-e4abae41740b'
+		images: {
+			barrier: 'https://firebasestorage.googleapis.com/v0/b/gopherwatch-664c5.appspot.com/o/zarya%2Fzarya-barrier.png?alt=media&token=4d7d6d83-7d0d-427c-a254-8199a1a4d5f3',
+			icon: 'https://firebasestorage.googleapis.com/v0/b/gopherwatch-664c5.appspot.com/o/zarya%2Fzarya-icon.png?alt=media&token=45ae8fb1-2d9b-42fd-a62b-2328ada070b1',
+			primary: 'https://firebasestorage.googleapis.com/v0/b/gopherwatch-664c5.appspot.com/o/zarya%2Fzarya-primary.png?alt=media&token=8d304904-f6da-4151-8523-b5f053b9f41a',
+			project: 'https://firebasestorage.googleapis.com/v0/b/gopherwatch-664c5.appspot.com/o/zarya%2Fzarya-project.png?alt=media&token=547ce503-0610-4f98-b144-ad2a8ccb4e33',
+			ult: 'https://firebasestorage.googleapis.com/v0/b/gopherwatch-664c5.appspot.com/o/zarya%2Fzarya-ult.png?alt=media&token=2d64d398-5ebf-4187-850c-abb4f43fb898'
+		}
 	},
 	"0x02E0000000000020": {
 		name: "Zenyatta",
@@ -85,7 +140,13 @@ export const Heroes = {
 	"0x02E000000000013B": {
 		name: "Ana",
 		color: 'rgb(204, 194, 174)',
-		ultIcon: 'https://firebasestorage.googleapis.com/v0/b/gopherwatch-664c5.appspot.com/o/0x0E60000000000185.png?alt=media&token=dec1c7a8-d318-4491-9001-e4abae41740b'
+		images: {
+			grenade: 'https://firebasestorage.googleapis.com/v0/b/gopherwatch-664c5.appspot.com/o/ana%2Fana-grenade.png?alt=media&token=132daf11-4ae2-469a-a841-4ba31be4c8d4',
+			icon: 'https://firebasestorage.googleapis.com/v0/b/gopherwatch-664c5.appspot.com/o/ana%2Fana-icon.png?alt=media&token=ac785597-1fec-41e5-8388-db58ea34b5d3',
+			primary: 'https://firebasestorage.googleapis.com/v0/b/gopherwatch-664c5.appspot.com/o/ana%2Fana-primary.png?alt=media&token=c052ca9f-74dd-43f6-b964-b8aa0e8598c7',
+			sleep: 'https://firebasestorage.googleapis.com/v0/b/gopherwatch-664c5.appspot.com/o/ana%2Fana-sleep.png?alt=media&token=dbebaf33-88e8-46cf-9f08-b22e4365d2a2',
+			nanoBoost: 'https://firebasestorage.googleapis.com/v0/b/gopherwatch-664c5.appspot.com/o/ana%2Fana-ult.png?alt=media&token=b0bc23f3-ef8d-4945-996d-0fe2bdc19066'
+		}
 	},
 	"0x02E0000000000006": {
 		name: "Torbjörn",
@@ -122,10 +183,197 @@ export const Heroes = {
 		color: 'rgb(141, 57, 57)',
 		ultIcon: 'https://firebasestorage.googleapis.com/v0/b/gopherwatch-664c5.appspot.com/o/0x0E60000000000174.png?alt=media&token=b329b9ea-72ba-4358-80f9-05f52aeb3e78'
 	},
-
 	"0x02E0000000000065": {
 		name: "Junkrat",
 		color: 'rgb(211, 147, 8)',
 		ultIcon: 'https://firebasestorage.googleapis.com/v0/b/gopherwatch-664c5.appspot.com/o/0x0E60000000000174.png?alt=media&token=b329b9ea-72ba-4358-80f9-05f52aeb3e78'
 	},
+}
+
+
+// HealingDone
+//  OffensiveAssists
+//  SelfHealing
+//  TurretDestroyed
+
+export const HeroDisplayDetails = {
+	"0x02E0000000000004": {
+		display: [
+			{
+				icon: 'icon',
+				statPaths: [
+					'Game.GamesPlayed',
+					'Game.GamesWon',
+					'Game.TimePlayed'
+				]
+			},
+			{
+				icon: 'heal',
+				statPaths: [
+					'Assists.HealingDone',
+					'Assists.OffensiveAssists',
+					'Assists.SelfHealing',
+					'Assists.TurretDestroyed',
+				]
+			},
+			{
+				icon: 'rez',
+				statPaths: [
+					'HeroSpecific.PlayersResurrected',
+					'HeroSpecific.PlayersResurrectedAverage',
+					'HeroSpecific.PlayersResurrectedMostinGame'
+				]
+			}
+		]
+	},
+	"0x02E0000000000003": {
+		display: [
+			{
+				icon: 'icon',
+				statPaths: [
+					'Game.GamesPlayed',
+					'Game.GamesWon',
+					'Game.TimePlayed'
+				]
+			},
+			{
+				icon: 'rewind',
+				statPaths: [
+					'Assists.SelfHealing',
+					'Assists.TurretsDestroyed',
+					'Assists.TeleporterPadsDestroyed'
+				]
+			},
+			{
+				icon: 'pulseBomb',
+				statPaths: [
+					'HeroSpecific.PulseBombKills',
+					'HeroSpecific.PulseBombKillsAverage',
+					'HeroSpecific.PulseBombKillsMostinGame',
+					'HeroSpecific.PulseBombsAttached',
+					'HeroSpecific.PulseBombsAttachedAverage',
+					'HeroSpecific.PulseBombsAttachedMostinGame',
+				]
+			}
+		]
+	},
+	"0x02E000000000007A": {
+		display: [
+			{
+				icon: 'icon',
+				statPaths: [
+					'Game.GamesPlayed',
+					'Game.GamesWon',
+					'Game.TimePlayed'
+				]
+			},
+			{
+				icon: 'selfDestruct',
+				statPaths: [
+					'Miscellaneous.SelfDestructKills',
+					'Miscellaneous.SelfDestructKillsAverage',
+					'Miscellaneous.SelfDestructKillsMostinGame',
+				]
+			},
+			{
+				icon: 'matrix',
+				statPaths: [
+					'HeroSpecific.DamageBlocked',
+					'HeroSpecific.DamageBlockedAverage',
+					'HeroSpecific.DamageBlockedMostinGame',
+				]
+			},
+			{
+				icon: 'mech',
+				statPaths: [
+					'HeroSpecific.MechDeaths',
+					'HeroSpecific.MechsCalled',
+					'HeroSpecific.MechsCalledAverage',
+					'HeroSpecific.MechsCalledMostinGame',
+				]
+			}
+		]
+	},
+	"0x02E000000000013B": {
+		display: [
+			{
+				icon: 'icon',
+				statPaths: [
+					'Game.GamesPlayed',
+					'Game.GamesWon',
+					'Game.TimePlayed'
+				]
+			},
+		]
+	},
+	"0x02E0000000000079": {
+		display: [
+			{
+				icon: 'icon',
+				statPaths: [
+					'Game.GamesPlayed',
+					'Game.GamesWon',
+					'Game.TimePlayed'
+				]
+			},
+		]
+	},
+	"0x02E0000000000007": {
+		display: [
+			{
+				icon: 'icon',
+				statPaths: [
+					'Game.GamesPlayed',
+					'Game.GamesWon',
+					'Game.TimePlayed'
+				]
+			},
+		]
+	},
+	"0x02E00000000000DD": {
+		display: [
+			{
+				icon: 'icon',
+				statPaths: [
+					'Game.GamesPlayed',
+					'Game.GamesWon',
+					'Game.TimePlayed'
+				]
+			}
+		]
+	},
+	"0x02E0000000000016": {
+		display: [
+			{
+				icon: 'icon',
+				statPaths: [
+					'Game.GamesPlayed',
+					'Game.GamesWon',
+					'Game.TimePlayed'
+				]
+			}
+		]
+	},
+	"0x02E0000000000068": {
+		display: [
+			{
+				icon: 'icon',
+				statPaths: [
+					'Game.GamesPlayed',
+					'Game.GamesWon',
+					'Game.TimePlayed'
+				]
+			}
+		]
+	}
+}
+
+
+
+
+export function GetHeroDetails ( id ) {
+	return {
+		hero: Heroes[ id ],
+		details: HeroDisplayDetails[ id ]
+	}
 }
